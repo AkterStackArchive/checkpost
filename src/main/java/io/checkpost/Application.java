b3bfec4a7e434c57c59c490a81dfa60c31fae108
shg@dsi <akter.hossain@dsinnovators.com>
2016-10-28 16:24:00 +0600

    updating...

---
+++ src/main/java/io/checkpost/Application.java
@@ -0,0 +1,12 @@
+package io.checkpost;
+
+import io.vertace.Vertace;
+
+
+public class Application extends Vertace {
+
+    public static void main(String[] args) {
+        Vertace.run(Application.class, args);
+    }
+
+}
