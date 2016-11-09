49f84ac94c68f34d9f30fbfb119904c496337ddb
shg@dsi <akter.hossain@dsinnovators.com>
2016-11-09 18:48:09 +0600

    namespace registration

--- src/main/java/io/checkpost/Application.java
+++ src/main/java/io/checkpost/Application.java
@@ -1,13 +1,13 @@
 package io.checkpost;
 
 import io.vertace.PackageScope;
-import io.vertace.Vertace;
+import io.vertace.VertaceWeb;
 
 @PackageScope("io.checkpost")
-public class Application extends Vertace {
+public class Application extends VertaceWeb {
 
     public static void main(String... args) {
-        Vertace.run(Application.class, args);
+        new Application().run(args);
     }
 
 }
