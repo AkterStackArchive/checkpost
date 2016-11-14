7fdc98f374c6128a3117f5fc93bfd1f1b6e9b410
shg@dsi <akter.hossain@dsinnovators.com>
2016-11-14 20:39:15 +0600

    refactoring

--- src/main/java/io/checkpost/Application.java
+++ src/main/java/io/checkpost/Application.java
@@ -1,13 +1,16 @@
 package io.checkpost;
 
 import io.vertace.PackageScope;
+import io.vertace.Vertace;
 import io.vertace.VertaceWeb;
+import io.vertx.core.Vertx;
 
 @PackageScope("io.checkpost")
 public class Application extends VertaceWeb {
 
     public static void main(String... args) {
-        new Application().run(args);
+        Vertace.deploy(new Application());
+
     }
 
 }
