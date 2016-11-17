96752a8f578ac17d4c12422d80ee5cf8b5ece7c4
shg@dsi <akter.hossain@dsinnovators.com>
2016-11-17 20:10:46 +0600

    refactoring

--- src/main/java/io/checkpost/CheckPostApplication.java
+++ src/main/java/io/checkpost/CheckPostApplication.java
@@ -3,11 +3,12 @@ package io.checkpost;
 import io.vertace.PackageScope;
 import io.vertace.Vertace;
 import io.vertace.VertaceWeb;
+import io.vertace.core.VertaceException;
 
 @PackageScope("io.checkpost")
 public class CheckPostApplication extends VertaceWeb {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws VertaceException {
         Vertace.deploy(new CheckPostApplication());
     }
 
--- src/main/java/io/checkpost/http/AuthenticationRouter.java
+++ src/main/java/io/checkpost/http/AuthenticationRouter.java
@@ -6,7 +6,9 @@ import io.vertace.http.HttpRestRouter;
 public class AuthenticationRouter extends HttpRestRouter<AuthenticationRouter> {
 
     {
-
+        get("/login", (req, res) -> {
+            res.done("sdfsdf");
+        });
     }
 
 }
